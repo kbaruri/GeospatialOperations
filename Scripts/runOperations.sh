4e5abeb85137f70b4bc0f29fab8996c362d9a652
Anshul Jain <anshul.jain1207@gmail.com>
2015-11-06 15:36:13 -0700

    Create runOperations.sh

---
+++ Scripts/runOperations.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/user22/Desktop/GeospatialOperations/GeospatialOperations/
+mvn package
+/usr/local/spark/bin/spark-submit --class "DDS.team22.GeospatialOperations.UnionPolygon" --master local[4] /home/user22/Desktop/GeospatialOperations/GeospatialOperations/target/GeospatialOperations-1.jar
+cd ~/
